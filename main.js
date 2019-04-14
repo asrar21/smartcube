15a9184cd4ba3116a76ffd6d9ecfd73dee0426fd
asrara <alamsyedmohammadasrar@gmail.com>
2019-04-14 11:11:51 +0500

    my practice typescript

---
+++ main.js
@@ -0,0 +1,50 @@
+//cohesive
+// class Vehicle{
+//     constructor( ){
+//         this.name='honda'
+//     }
+// }
+// class Car extends Vehicle{
+// }
+// var vehicle=new Vehicle();
+// // var first=hero.getfirstname();
+// // var last=hero.getlastname();
+// // prompt("firstname and age is  : ",first);
+// // prompt("lastname is :",last)
+// // var wifes=new wife("mahnoor","Firoze");
+// // var wname=wifes.child();
+// // console.log(wname);
+// let number =[1,2,3,4,5,6],newnum=[...number,7,8,7];
+// console.log("random",number);//[1,2,3]
+// console.log("Mult",mult);
+// console.log("newnumber",newnum);//[2,4,6]
+var person = "instructor";
+console.log(person);
+person = "UI Developer";
+console.log(person);
+var str = "Asrar is a UI 'Engineer'";
+console.log(str);
+var number = "27.89";
+console.log(number);
+var num1 = 20, num2 = 10;
+var a = (5 + 5) * 5;
+console.log(a);
+var firstName = "asrar", lastName = "alam", age = "24";
+//if else if stataments
+var myname = "Asrar";
+var prof = "teacher";
+if (prof === "engineer") {
+    console.log(prof + "works in workshop");
+}
+else if (prof === "doctor") {
+    console.log(prof + "treates paitents");
+}
+else if (prof === "teacher") {
+    console.log(prof + "teaches students");
+}
+if (5 === 4 || 4 === 4) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
---
+++ main.ts
@@ -0,0 +1,69 @@
+
+
+//cohesive
+// class Vehicle{
+    
+//     constructor( ){
+//         this.name='honda'
+
+//     }
+    
+
+// }
+// class Car extends Vehicle{
+    
+
+// }
+// var vehicle=new Vehicle();
+// // var first=hero.getfirstname();
+// // var last=hero.getlastname();
+// // prompt("firstname and age is  : ",first);
+// // prompt("lastname is :",last)
+// // var wifes=new wife("mahnoor","Firoze");
+// // var wname=wifes.child();
+// // console.log(wname);
+
+// let number =[1,2,3,4,5,6],newnum=[...number,7,8,7];
+
+
+// console.log("random",number);//[1,2,3]
+// console.log("Mult",mult);
+// console.log("newnumber",newnum);//[2,4,6]
+
+let person="instructor";
+console.log(person);
+person="UI Developer";
+console.log(person)
+let str="Asrar is a UI 'Engineer'"
+console.log(str)
+let number="27.89";
+console.log(number)
+let num1=20,num2=10;
+
+
+let a=(5+5)*5;
+console.log(a)
+let firstName="asrar",
+lastName="alam",
+age="24"
+//if else if stataments
+let myname="Asrar";
+let prof="teacher";
+
+if(prof==="engineer"){
+    console.log(prof+"works in workshop")
+}
+else if(prof==="doctor"){
+    console.log(prof + "treates paitents")
+}
+else if(prof==="teacher"){
+    console.log(prof +"teaches students")
+}
+
+if(5 === 4 || 4 === 4 ){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
